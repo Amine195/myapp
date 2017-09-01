b04c7326c42a611eac1ea32fc3178dae682decb9
Amine195 <gmadhgs@gmail.com>
2017-09-01 14:49:41 +0100

    new Commit

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ function remove(){
 setInterval(remove,24*60*60*1000);
 
 // Server Runninig
-app.listen(3000, function(){
-    console.log('Listening on port 3000');
+app.listen(4200, function(){
+    console.log('Listening on port 4200');
 });
